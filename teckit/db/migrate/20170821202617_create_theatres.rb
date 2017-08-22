bfb7d1c86266d66ed6a26ca1d968e45e8d79e292
kewoodh <kesean.woodhouse@gmail.com>
2017-08-22 09:07:45 -0400

    first init

--- teckit/db/migrate/20170821202617_create_theatres.rb
+++ teckit/db/migrate/20170821202617_create_theatres.rb
@@ -1,9 +1,8 @@
 class CreateTheatres < ActiveRecord::Migration[5.1]
   def change
     create_table :theatres do |t|
-      t.string, :name
+      t.string :name
       t.integer :city_id
-
       t.timestamps
     end
   end
--- teckit/db/migrate/20170821203436_create_showtimes.rb
+++ teckit/db/migrate/20170821203436_create_showtimes.rb
@@ -2,7 +2,7 @@ class CreateShowtimes < ActiveRecord::Migration[5.1]
   def change
     create_table :showtimes do |t|
       t.datetime :start_time
-      t.date_time :end_time
+      t.datetime :end_time
       t.integer :movie_id
       t.integer :theatre_id
 

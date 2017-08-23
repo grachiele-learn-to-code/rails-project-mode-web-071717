27c97d048ef494ae541ce405765acf1679f15ef9
kewoodh <kesean.woodhouse@gmail.com>
2017-08-23 10:13:25 -0400

    fixing routes to account for movie_theare paths

--- teckit/config/routes.rb
+++ teckit/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cities, :genres, :movie_theatres, :movies, only: [:index, :show]
+  resources :cities, :genres, :movies, only: [:index, :show]
   resources :users
+
   root to: 'welcome#index'
+  get '/movietheatres', to: 'movie_theatres#index'
+  get '/movietheatres/:id', to: 'movie_theatres#show'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/signin', to: 'sessions#new'
